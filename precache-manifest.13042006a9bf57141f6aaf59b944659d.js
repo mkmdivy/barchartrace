cb5b3b4aaa9d91857212c2e02380d1a3d98ad2b9
mkmdivy <mkmdivy@gmail.com>
2022-04-15 12:25:28 +0200

    Updates

---
+++ precache-manifest.13042006a9bf57141f6aaf59b944659d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ce77adabd6e05c41477e980a1486d4",
+    "url": "/barchartrace/index.html"
+  },
+  {
+    "revision": "6c744e3d992d3daa861d",
+    "url": "/barchartrace/static/css/main.eb325980.chunk.css"
+  },
+  {
+    "revision": "c811e2831cd117a9a1cc",
+    "url": "/barchartrace/static/js/2.9549a0f7.chunk.js"
+  },
+  {
+    "revision": "e037bed7d464e9ab61da740779a12f56",
+    "url": "/barchartrace/static/js/2.9549a0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c744e3d992d3daa861d",
+    "url": "/barchartrace/static/js/main.4ae981bd.chunk.js"
+  },
+  {
+    "revision": "9e3fb9fd9cc6bb7c4393",
+    "url": "/barchartrace/static/js/runtime-main.05ed2490.js"
+  }
+]);
