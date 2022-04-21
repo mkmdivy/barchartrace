1d7e2a2330d21a687fa9c8cd5fd41deb8888b0ad
mkmdivy <mkmdivy@gmail.com>
2022-04-21 14:56:20 +0200

    Updates

---
+++ precache-manifest.f730ce22ea166779c7000a33f4b4afc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b22128de94384046d747a3cb9882df5",
+    "url": "/barchartrace/index.html"
+  },
+  {
+    "revision": "257ddca0604196cded21",
+    "url": "/barchartrace/static/css/2.010f233d.chunk.css"
+  },
+  {
+    "revision": "a97a66bbf02d7ce03e07",
+    "url": "/barchartrace/static/css/main.ef069f7a.chunk.css"
+  },
+  {
+    "revision": "257ddca0604196cded21",
+    "url": "/barchartrace/static/js/2.fe7f8834.chunk.js"
+  },
+  {
+    "revision": "e037bed7d464e9ab61da740779a12f56",
+    "url": "/barchartrace/static/js/2.fe7f8834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97a66bbf02d7ce03e07",
+    "url": "/barchartrace/static/js/main.950a5b92.chunk.js"
+  },
+  {
+    "revision": "9e3fb9fd9cc6bb7c4393",
+    "url": "/barchartrace/static/js/runtime-main.05ed2490.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/barchartrace/precache-manifest.b09c704fb51952e92d0d5884268d588e.js"
+  "/barchartrace/precache-manifest.f730ce22ea166779c7000a33f4b4afc3.js"
 );
 
 self.addEventListener('message', (event) => {
